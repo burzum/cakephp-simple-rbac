71f04dd64457cda2412137a2a6898c812d8c1afd
Florian Krämer <florian.kraemer@kreative-design.net>
2019-11-28 15:51:55 +0100

    Coding standard updates

--- src/Auth/SimpleRbacAuthorize.php
+++ src/Auth/SimpleRbacAuthorize.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Burzum\SimpleRbac\Auth;
 
 use Cake\Auth\BaseAuthorize;
@@ -16,170 +17,178 @@ use RuntimeException;
  * @copyright Copyright 2011 - 2015, Florian Krämer
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-class SimpleRbacAuthorize extends BaseAuthorize {
+class SimpleRbacAuthorize extends BaseAuthorize
+{
 
-	/**
-	 * Default config for this object.
-	 *
-	 * - `roleField` - The name of the role field in the user data array that is passed to authorize()
-	 *
-	 * @var array
-	 */
-	public $_defaultConfig = array(
-		'roleField' => 'role',
-		'allowEmptyActionMap' => false,
-		'allowEmptyPrefixMap' => true,
-		'undefinedActionsAreAllowed' => false
-	);
+    /**
+     * Default config for this object.
+     *
+     * - `roleField` - The name of the role field in the user data array that is passed to authorize()
+     *
+     * @var array
+     */
+    public $_defaultConfig = array(
+        'roleField' => 'role',
+        'allowEmptyActionMap' => false,
+        'allowEmptyPrefixMap' => true,
+        'undefinedActionsAreAllowed' => false
+    );
 
-	/**
-	 * Authorize a user based on his roles
-	 *
-	 * @param array $user The user to authorize
-	 * @param Request $request The request needing authorization.
-	 * @return boolean
-	 * @throws RuntimeException when the role field does not exist
-	 */
-	public function authorize($user, ServerRequest $request) {
-		$roleField = $this->_config['roleField'];
+    /**
+     * Authorize a user based on his roles
+     *
+     * @param array $user The user to authorize
+     * @param Request $request The request needing authorization.
+     * @return boolean
+     * @throws RuntimeException when the role field does not exist
+     */
+    public function authorize($user, ServerRequest $request)
+    {
+        $roleField = $this->_config['roleField'];
 
-		if (!isset($user[$roleField])) {
-			throw new RuntimeException(sprintf('The role field `%s` does not exist!', $roleField));
-		}
+        if (!isset($user[$roleField])) {
+            throw new RuntimeException(sprintf('The role field `%s` does not exist!', $roleField));
+        }
 
-		if (is_string($user[$roleField])) {
-			$user[$roleField] = array($user[$roleField]);
-		}
+        if (is_string($user[$roleField])) {
+            $user[$roleField] = array($user[$roleField]);
+        }
 
-		if ($this->authorizeByPrefix($user[$roleField], $request)) {
-			return true;
-		}
+        if ($this->authorizeByPrefix($user[$roleField], $request)) {
+            return true;
+        }
 
-		if ($this->authorizeByControllerAndAction($user, $request)) {
-			return true;
-		}
+        if ($this->authorizeByControllerAndAction($user, $request)) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Checks if a role is granted access to a controller and action
-	 *
-	 * @param array $user
-	 * @param Request $request
-	 * @return boolean
-	 */
-	public function authorizeByControllerAndAction($user, ServerRequest $request) {
-		$roleField = $this->getConfig('roleField');
-		extract($this->getControllerNameAndAction($request));
-		$actionMap = $this->getActionMap();
+    /**
+     * Checks if a role is granted access to a controller and action
+     *
+     * @param array $user
+     * @param Request $request
+     * @return boolean
+     */
+    public function authorizeByControllerAndAction($user, ServerRequest $request)
+    {
+        $roleField = $this->getConfig('roleField');
+        extract($this->getControllerNameAndAction($request));
+        $actionMap = $this->getActionMap();
 
-		if (isset($actionMap[$name]['*'])) {
-			if ($this->_isAllowedRole($user[$roleField], $actionMap[$name]['*'])) {
-				return true;
-			}
-		}
+        if (isset($actionMap[$name]['*'])) {
+            if ($this->_isAllowedRole($user[$roleField], $actionMap[$name]['*'])) {
+                return true;
+            }
+        }
 
-		if (isset($actionMap[$name][$action])) {
-			if ($this->_isAllowedRole($user[$roleField], $actionMap[$name][$action])) {
-				return true;
-			}
-		}
+        if (isset($actionMap[$name][$action])) {
+            if ($this->_isAllowedRole($user[$roleField], $actionMap[$name][$action])) {
+                return true;
+            }
+        }
 
-		if ($this->getConfig('undefinedActionsAreAllowed') === true) {
-			return true;
-		}
+        if ($this->getConfig('undefinedActionsAreAllowed') === true) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Checks if a role is allowed.
-	 *
-	 * @param array|string $userRoles
-	 * @param array $allowedRoles
-	 * @return boolean
-	 */
-	protected function _isAllowedRole($userRoles, array $allowedRoles) {
-		if (in_array('*', $allowedRoles)) {
-			return true;
-		}
-		if (is_string($userRoles)) {
-			$userRoles = [$userRoles];
-		}
-		foreach ($userRoles as $userRole) {
-			if (in_array($userRole, $allowedRoles)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Checks if a role is allowed.
+     *
+     * @param array|string $userRoles
+     * @param array $allowedRoles
+     * @return boolean
+     */
+    protected function _isAllowedRole($userRoles, array $allowedRoles)
+    {
+        if (in_array('*', $allowedRoles)) {
+            return true;
+        }
+        if (is_string($userRoles)) {
+            $userRoles = [$userRoles];
+        }
+        foreach ($userRoles as $userRole) {
+            if (in_array($userRole, $allowedRoles)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * Checks if a role is granted access to a prefix route like /admin.
-	 *
-	 * @param array $roles
-	 * @param Request $request
-	 * @return boolean
-	 */
-	public function authorizeByPrefix(array $roles, ServerRequest $request) {
-		$prefixeMap = $this->getPrefixMap();
-		$prefix = $request->getParam('prefix');
-		if ($prefix !== null && isset($prefixeMap[$prefix])) {
-			foreach ($roles as $role) {
-				if (in_array($role, $prefixeMap[$prefix])) {
-					return true;
-				}
-			}
-		}
+    /**
+     * Checks if a role is granted access to a prefix route like /admin.
+     *
+     * @param array $roles
+     * @param Request $request
+     * @return boolean
+     */
+    public function authorizeByPrefix(array $roles, ServerRequest $request)
+    {
+        $prefixeMap = $this->getPrefixMap();
+        $prefix = $request->getParam('prefix');
+        if ($prefix !== null && isset($prefixeMap[$prefix])) {
+            foreach ($roles as $role) {
+                if (in_array($role, $prefixeMap[$prefix])) {
+                    return true;
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Gets the controller and action, prefixes the controller with the plugin if there is one
-	 *
-	 * @param Request $request
-	 * @return array
-	 */
-	public function getControllerNameAndAction(ServerRequest $request) {
-		$controller = $this->_registry->getController();
-		$name = $controller->getName();
-		$action = $request->getParam('action');
+    /**
+     * Gets the controller and action, prefixes the controller with the plugin if there is one
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function getControllerNameAndAction(ServerRequest $request)
+    {
+        $controller = $this->_registry->getController();
+        $name = $controller->getName();
+        $action = $request->getParam('action');
         $plugin = $request->getParam('plugin');
 
-		if (!empty($plugin)) {
-			$name = Inflector::camelize($plugin) . '.' . $name;
-		}
+        if (!empty($plugin)) {
+            $name = Inflector::camelize($plugin) . '.' . $name;
+        }
 
-		return compact('name', 'action');
-	}
+        return compact('name', 'action');
+    }
 
-	/**
-	 * Can be overridden if inherited with a method to fetch this from anywhere, a database for example.
-	 *
-	 * @return array
-	 * @throws RuntimeException
-	 */
-	public function getActionMap() {
-		$actionMap = (array) Configure::read('SimpleRbac.actionMap');
-		if (empty($actionMap) && $this->getConfig('allowEmptyActionMap') === false) {
-			throw new RuntimeException('SimpleRbac.actionMap configuration is empty!');
-		}
-		return $actionMap;
-	}
+    /**
+     * Can be overridden if inherited with a method to fetch this from anywhere, a database for example.
+     *
+     * @return array
+     * @throws RuntimeException
+     */
+    public function getActionMap()
+    {
+        $actionMap = (array) Configure::read('SimpleRbac.actionMap');
+        if (empty($actionMap) && $this->getConfig('allowEmptyActionMap') === false) {
+            throw new RuntimeException('SimpleRbac.actionMap configuration is empty!');
+        }
+        return $actionMap;
+    }
 
-	/**
-	 * Can be overriden if inherited with a method to fetch this from anywhere, a database for exaple
-	 *
-	 * @return array
-	 * @throws \RuntimeException
-	 */
-	public function getPrefixMap() {
-		$prefixMap = (array) Configure::read('SimpleRbac.prefixMap');
-		if (empty($prefixMap) && $this->getConfig('allowEmptyPrefixMap') === false) {
-			throw new RuntimeException('SimpleRbac.prefixMap configuration is empty!');
-		}
-		return $prefixMap;
-	}
+    /**
+     * Can be overriden if inherited with a method to fetch this from anywhere, a database for exaple
+     *
+     * @return array
+     * @throws \RuntimeException
+     */
+    public function getPrefixMap()
+    {
+        $prefixMap = (array) Configure::read('SimpleRbac.prefixMap');
+        if (empty($prefixMap) && $this->getConfig('allowEmptyPrefixMap') === false) {
+            throw new RuntimeException('SimpleRbac.prefixMap configuration is empty!');
+        }
+        return $prefixMap;
+    }
 }
--- tests/TestCase/Auth/SimpleRbacAuthorizeTest.php
+++ tests/TestCase/Auth/SimpleRbacAuthorizeTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Burzum\SimpleRbac\Test\TestCase\Event;
 
 use Burzum\SimpleRbac\Auth\SimpleRbacAuthorize;
@@ -18,93 +19,97 @@ use Cake\TestSuite\TestCase;
  * @copyright Copyright 2011 - 2015, Florian Krämer
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-class SimpleRbacAuthorizeTest extends TestCase {
+class SimpleRbacAuthorizeTest extends TestCase
+{
 
-	/**
-	 * setup
-	 *
-	 * @return void
-	 */
-	public function setUp() {
-		parent::setUp();
+    /**
+     * setup
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
 
-		$this->controller = $this->getMockBuilder(Controller::class)
+        $this->controller = $this->getMockBuilder(Controller::class)
             ->setMethods(['isAuthorized'])
             ->disableOriginalConstructor()
             ->getMock();
 
-		$this->components = $this->getMockBuilder(ComponentRegistry::class)
+        $this->components = $this->getMockBuilder(ComponentRegistry::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-		$this->components->expects($this->any())
-			->method('getController')
-			->will($this->returnValue($this->controller));
+        $this->components->expects($this->any())
+            ->method('getController')
+            ->will($this->returnValue($this->controller));
 
-		$this->auth = new SimpleRbacAuthorize($this->components);
+        $this->auth = new SimpleRbacAuthorize($this->components);
 
-		$actionMap = array(
-			'Rbac.Roles' => array(
-				'index' => array('*'),
-				'add' => array('admin'),
-				'edit' => []
-			)
-		);
+        $actionMap = array(
+            'Rbac.Roles' => array(
+                'index' => array('*'),
+                'add' => array('admin'),
+                'edit' => []
+            )
+        );
 
-		Configure::write('SimpleRbac.actionMap', $actionMap);
-	}
+        Configure::write('SimpleRbac.actionMap', $actionMap);
+    }
 
-	/**
-	 * test failure
-	 *
-	 * @return void
-	 */
-	public function testAuthorizeFailure() {
-		$this->controller->name = 'Roles';
-		$this->controller->action = 'edit';
+    /**
+     * test failure
+     *
+     * @return void
+     */
+    public function testAuthorizeFailure()
+    {
+        $this->controller->name = 'Roles';
+        $this->controller->action = 'edit';
 
-		$user = array(
-			'role' => 'admin',
-			'id' => '4316da10-4014-4640-8df2-05c2c0a80b96'
-		);
+        $user = array(
+            'role' => 'admin',
+            'id' => '4316da10-4014-4640-8df2-05c2c0a80b96'
+        );
 
-		$request = new Request('/rbac/roles/edit');
-		$request->params['plugin'] = 'rbac';
-		$this->assertFalse($this->auth->authorize($user, $request));
-	}
+        $request = new Request('/rbac/roles/edit');
+        $request->params['plugin'] = 'rbac';
+        $this->assertFalse($this->auth->authorize($user, $request));
+    }
 
-	/**
-	 * test isAuthorized working.
-	 *
-	 * @return void
-	 */
-	public function testAuthorizeSuccess() {
-		$user = array(
-			'role' => 'admin'
-		);
+    /**
+     * test isAuthorized working.
+     *
+     * @return void
+     */
+    public function testAuthorizeSuccess()
+    {
+        $user = array(
+            'role' => 'admin'
+        );
 
-		$this->controller->name = 'Roles';
-		$this->controller->action = 'add';
-		$request = new Request('/rbac/roles/add');
-		$request->action = 'add';
-		$request->controller = 'Roles';
-		$request->params = [
-			'plugin' => 'Rbac',
-			'controller' => 'Roles',
-			'action' => 'add'
-		];
-		$this->assertTrue($this->auth->authorize($user, $request));
+        $this->controller->name = 'Roles';
+        $this->controller->action = 'add';
+        $request = new Request('/rbac/roles/add');
+        $request->action = 'add';
+        $request->controller = 'Roles';
+        $request->params = [
+            'plugin' => 'Rbac',
+            'controller' => 'Roles',
+            'action' => 'add'
+        ];
+        $this->assertTrue($this->auth->authorize($user, $request));
 
-		$this->controller->name = 'Roles';
-		$this->controller->action = 'index';
-		$request = new Request('/rbac/roles/index');
-		$request->action = 'index';
-		$request->controller = 'Roles';
-		$request->params = [
-			'plugin' => 'Rbac',
-			'controller' => 'Roles',
-			'action' => 'index'
-		];
-		$this->assertTrue($this->auth->authorize($user, $request));
-	}
+        $this->controller->name = 'Roles';
+        $this->controller->action = 'index';
+        $request = new Request('/rbac/roles/index');
+        $request->action = 'index';
+        $request->controller = 'Roles';
+        $request->params = [
+            'plugin' => 'Rbac',
+            'controller' => 'Roles',
+            'action' => 'index'
+        ];
+        $this->assertTrue($this->auth->authorize($user, $request));
+    }
 }
